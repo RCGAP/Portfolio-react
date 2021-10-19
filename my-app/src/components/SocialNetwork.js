ac70310a4393cfde8e18ad692c866f32c05df71f
RCGAP <Garnetangeldust@gmail.com>
2021-10-19 18:51:25 +0200

    test

---
+++ my-app/src/components/SocialNetwork.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const SocialNetwork = () => {
+
+    // Animation des icones des réseaux sociaux.
+  const anim = () => {
+    let navLinks = document.querySelectorAll('.social-network a');
+
+    navLinks.forEach(link => {
+      link.addEventListener('mouseover', (e) => {
+        let attrX = e.offsetX - 20;
+        let attrY = e.offsetY - 13;
+
+        link.style.transform = `translate( ${attrX}px, ${attrY}px)`;
+      })
+      link.addEventListener('mouseleave', () => {
+        link.style.transform = '';
+      })
+    })
+  }
+    // Lien réseaux sociaux
+  return (
+    <div className="social-network">
+      <ul className="content">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
+          <li><i className="fab fa-facebook-f"></i></li>
+        </a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
+          <li><i className="fab fa-twitter"></i></li>
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer" className="hover" onMouseOver={anim} >
+          <li><i className="fab fa-instagram"></i></li>
+        </a>
+      </ul>
+    </div>
+  );
+};
+
+export default SocialNetwork;
